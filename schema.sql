359a1b789015a743e3ad2eb8fd56dc89df685f87
shangshujin <shangshujin@columbia.edu>
2022-12-03 23:47:09 +0800

    update Part2

--- schema.sql
+++ schema.sql
@@ -1,20 +1,18 @@
 CREATE TABLE IF NOT EXISTS hourly_weather (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     DATE DATE,
-    HourlyWindSpeed INT8,
-    HourlyPrecipitation FLOAT64
+    HourlyWindSpeed FLOAT32,
+    HourlyPrecipitation FLOAT32
 );
 CREATE TABLE IF NOT EXISTS daily_weather (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    DATE DATE ,
-    DailyWindSpeed INT8,
-    DailyPrecipitation FLOAT64
+    DATE DATE,
+    DailyWindSpeed FLOAT32,
+    DailyPrecipitation FLOAT32
 );
 CREATE TABLE IF NOT EXISTS taxi_trips (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     pickup_datetime DATE,
-    PULocationID INT8,
-    DOLocationID INT8,
     pickup_longitude FLOAT32,
     pickup_latitude FLOAT32,
     dropoff_longitude FLOAT32,
@@ -30,4 +28,10 @@ CREATE TABLE IF NOT EXISTS uber_trips (
     dropoff_longitude FLOAT32,
     dropoff_latitude FLOAT32,
     distance FLOAT32
+);
+CREATE TABLE IF NOT EXISTS sun_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    DATE DATE,
+    Sunrise INT32,
+    Sunset INT32
 );
